ec1f5083ff8980459f0c19fc3467bd41217a973d
alandry <aaron.llandry@gmail.com>
2016-10-05 17:31:22 -0500

    adding parallax, smooth scroll and sticky elements

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -33,9 +33,25 @@ $(function () {
 		}
 	});
 	
+	// PARALLAX
 	if ($('.parallax').length > 0) {
 		$('.parallax').parallax();
 	}
+	
+	// SMOOTH SCROLL
+	$(".scroll").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+		 var destOffset = 0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest-destOffset}, 1000,'swing');
+     });
 	
 	
 	// LAYOUT
